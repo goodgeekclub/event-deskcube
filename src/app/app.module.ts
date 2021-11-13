96088aae78cd161bc4554b5fe6ecfa626845db05
Araiva <kizzaraiva@gmail.com>
2021-11-13 22:50:53 +0700

    test mat card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import {SharedComponentsModule} from './shared-components/shared-components.modu
 import { HomeObjectiveComponent } from './containers/home/home-objective/home-objective.component';
 import { HomeWhatComponent } from './containers/home/home-what/home-what.component';
 import { HomeCoverComponent } from './containers/home/home-cover/home-cover.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -27,6 +29,8 @@ import { HomeCoverComponent } from './containers/home/home-cover/home-cover.comp
     BrowserAnimationsModule,
     FlexLayoutModule,
     SharedComponentsModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
