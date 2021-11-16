e429ef30de2d43137fa0d9a1c467012590e67462
Araiva <kizzaraiva@gmail.com>
2021-11-16 17:59:06 +0700

    fix script bug

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -1,22 +1,27 @@
+# Setup Variable
+aws_profile="gg-admin"
+dist="dist/gg-deskcube/browser"
+bucket="s3://deskcube.io"
+cfId="E2K94A2WMRDWO"
 
-export AWS_PROFILE=gg-admin
-echo "=== TEST List AWS_S33 ==="
+# Script
+export AWS_PROFILE=$aws_profile
+echo "=== TEST List AWS_S3 ==="
 aws s3 ls
-echo "=== TEST List AWS_S33 ==="
+echo "=== TEST List AWS_S3 ==="
 for i in {1..10}; do
   printf '.'
   sleep 1
 done
 
 echo "=== Angular build Pre-Render ==="
-
 npm run prerender
-
-aws s3 sync dist/gg-deskcube/browser deskcube.io --delete
-#cp docs/index.html docs/404.html
+aws s3 sync $dist $bucket --delete
 echo "AWS S3 is synced"
 echo "Waiting S3 Cached for 5 second"
 sleep 5
 echo "Invalidate Cloudfront"
-aws cloudfront create-invalidation --distribution-id E2K94A2WMRDWO --paths "/"
+aws cloudfront create-invalidation --distribution-id $cfId --paths "/*"
 echo "=== Finish Deploy==="
+
+# Finish
