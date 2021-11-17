6c96be6935af7f7a5e6714c79a17705cd962e2d2
Araiva <kizzaraiva@gmail.com>
2021-11-18 01:08:40 +0700

    fix nav menu

--- src/app/shared-components/nabvbar/navbar.component.ts
+++ src/app/shared-components/nabvbar/navbar.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  links = [
+    { title: 'Home', location: '/', hashtag: undefined },
+    { title: 'Agenda', location: '/', hashtag: 'agenda' },
+    { title: 'Speakers', location: '/speakers', hashtag: undefined },
+    { title: 'Location', location: '/', hashtag: 'location' },
+    { title: 'Pricing', location: '/', hashtag: 'pricing' },
+    { title: 'Contact Us', location: '/', hashtag: 'footer' },
+    { title: 'Staff', location: '/staff', hashtag: undefined },
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  clickMenu() {
+    console.log('CLICK');
+  }
 }
