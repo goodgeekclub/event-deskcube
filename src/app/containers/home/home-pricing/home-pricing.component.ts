b0f726e0a9baca638293307d79b823f926ee5980
Araiva <kizzaraiva@gmail.com>
2021-11-18 15:09:17 +0700

    minor fix btn

--- src/app/containers/home/home-pricing/home-pricing.component.ts
+++ src/app/containers/home/home-pricing/home-pricing.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePricingComponent implements OnInit {
 
+  cards = [
+    {title: 'Free', desc: 'สำหรับผู้สมัคร 5 ท่านแรก', color: 'var(--my-canary)'},
+    {title: '159', desc: 'ทำภารกิจพิเศษ', color: '#928A97'},
+    {title: '199', desc: 'เข้าร่วมแบบไร้เงื่อนไข', color: '#5C718C'},
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
